362bbdfd788cc8acc7af8be6b63a14554da6b3a8
sam-bhav <unique20052003@gmail.com>
2023-02-06 17:08:19 +0545

    testing some built in functions

---
+++ code/binary2decimal.py
@@ -0,0 +1,13 @@
+# x=int(input("Enter a number "))
+# y=bin(x)
+# print(y[2:])
+
+x=input("Enter binary number ")
+dec_number=int(x,2)
+
+try:
+    
+    if  x:
+        print("correct")
+except:
+    print("Invalid")
---
+++ code/test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main_function(){
+    int number;
+    cin>>number;
+    return (number*2);
+}
